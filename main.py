ab04e49ebed545884ad044f54b490aa203ea0e1d
chigaevi <chigaev.i@gmail.com>
2022-08-02 23:12:46 +0700

    commit 2

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+from pprint import pprint
+
 with open('recipes.txt', encoding='utf-8') as recipe_text:
     cook_book = {}
     for line in recipe_text:
         dish_name = line.strip()
         quantity_ing = int(recipe_text.readline().strip())
         ing_list = []
-        ing_str = {}
+        ing_dic = {}
         for line in range(quantity_ing):
             ing = recipe_text.readline().split('|')
-            ing_str['ingredient_name'] = ing[0].strip()
-            ing_str['quantity'] = ing[1].strip()
-            ing_str['measure'] = ing[2].strip()
-            ing_list.append(ing_str)
+            ing_dic['ingredient_name'] = ing[0].strip()
+            ing_dic['quantity'] = ing[1].strip()
+            ing_dic['measure'] = ing[2].strip()
+            ing_list.append(ing_dic.copy())
         cook_book[dish_name] = ing_list
         recipe_text.readline()
-    print(cook_book)
+    pprint(cook_book)
